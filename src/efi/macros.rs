25ac19cc15db98948b8a2f7586bc8ee0e2879ff9
Sergei Fomin <sergio-dna@yandex.ru>
2018-10-22 12:21:10 +0300

    Get rif of efi_main macro

--- src/efi/macros.rs
+++ src/efi/macros.rs
@@ -1,30 +1,5 @@
 #![macro_use]
 
-macro_rules! efi_main {
-    ($(fn main($handle:ident: $htype:ty, $table:ident: $stype:ty)
-            -> $ret:ty $body:block)+) => ($(
-
-        fn main($handle: $htype, $table: $stype) -> $ret
-            $body
-
-        #[no_mangle]
-        pub extern "C" fn efi_main(
-            handle: efi::ctypes::EFI_HANDLE,
-            st: *const efi::ctypes::EFI_SYSTEM_TABLE
-        ) -> ! {
-            unsafe {
-                efi::ffi::InitializeLib(handle, st);
-            }
-            let table = unsafe { efi::SystemTable::claim(st) };
-            let status = main(handle, table);
-            print!(b"Loader exited. Status: %r.\n",
-                   status as efi::ctypes::EFI_STATUS);
-            loop {}
-        }
-    )+);
-
-}
-
 macro_rules! wstr {
     ( $s:literal ) => {{
         let mut buf: [u16; $s.len() + 1] = unsafe { core::mem::uninitialized() };
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@
 #![feature(macro_literal_matcher)]
 
 extern crate alloc;
+extern crate spin;
 extern crate linked_list_allocator;
 
 mod efi;
@@ -20,39 +21,54 @@ use efi::{EfiHandle, EfiHeap, EfiStatus, SystemTable};
 use kernel_handler::KernelHandler;
 use memory_map::{MemoryMap, MemoryQuery};
 
+const HEAP_PAGES: usize = 10;
 const KERNEL_IMAGE_PATH: &[u8] = b"\\EFI\\BOOT\\KERNEL.IMG";
 
 #[global_allocator]
 static ALLOCATOR: EfiHeap = EfiHeap::empty();
 
-efi_main! {
-    fn main(handle: EfiHandle, table: &SystemTable) -> EfiStatus {
-        unsafe { ALLOCATOR.init(table, 10); }
-
-        print!(b"UEFI OS loader started.\n");
-
-        print!(b"Getting memory map... ");
-        let memmap = MemoryMap::current(table)?;
-        print!(b"done.\n");
-
-        print!(b"Looking for free space... ");
-        let kseg = memmap.find_segment(MemoryQuery::KthBiggest(0))?;
-        let sseg = memmap.find_segment(MemoryQuery::KthBiggest(1))?;
-        print!(b"found.\n");
-
-        print!(b"Loading kernel image... ");
-        let khandler = KernelHandler::load_image(KERNEL_IMAGE_PATH,
-                                                 kseg.start,
-                                                 kseg.size)?;
-        print!(b"done.\n");
-
-        print!(b"Exiting boot services... ");
-        let memmap = exit_boot_services(handle, table, memmap)?;
-        print!(b"done.\n");
-
-        print!(b"Jumping into the kernel. Goodbye, UEFI!\n");
-        khandler.jump(sseg.start, table, memmap); // never returns
+#[no_mangle]
+pub extern "C" fn efi_main(
+    handle: efi::ctypes::EFI_HANDLE,
+    st: *const efi::ctypes::EFI_SYSTEM_TABLE,
+) -> ! {
+    unsafe {
+        efi::ffi::InitializeLib(handle, st);
     }
+    let table = unsafe { efi::SystemTable::claim(st) };
+    unsafe {
+        ALLOCATOR.init(table, HEAP_PAGES);
+    }
+    let status = main(handle, table);
+    print!(
+        b"Loader exited. Status: %r.\n",
+        status as efi::ctypes::EFI_STATUS
+    );
+    loop {}
+}
+
+fn main(handle: EfiHandle, table: &SystemTable) -> EfiStatus {
+    print!(b"UEFI OS loader started.\n");
+
+    print!(b"Getting memory map... ");
+    let memmap = MemoryMap::current(table)?;
+    print!(b"done.\n");
+
+    print!(b"Looking for free space... ");
+    let kseg = memmap.find_segment(MemoryQuery::KthBiggest(0))?;
+    let sseg = memmap.find_segment(MemoryQuery::KthBiggest(1))?;
+    print!(b"found.\n");
+
+    print!(b"Loading kernel image... ");
+    let khandler = KernelHandler::load_image(KERNEL_IMAGE_PATH, kseg.start, kseg.size)?;
+    print!(b"done.\n");
+
+    print!(b"Exiting boot services... ");
+    let memmap = exit_boot_services(handle, table, memmap)?;
+    print!(b"done.\n");
+
+    print!(b"Jumping into the kernel. Goodbye, UEFI!\n");
+    khandler.jump(sseg.start, table, memmap); // never returns
 }
 
 fn exit_boot_services(
