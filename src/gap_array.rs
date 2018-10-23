f04e14bb392e62bb5566083b520085d9e4678ea3
Sergei Fomin <sergio-dna@yandex.ru>
2018-10-23 11:24:02 +0300

    Implement iteration on GapArray

--- src/gap_array.rs
+++ src/gap_array.rs
@@ -1,4 +1,5 @@
 use alloc::prelude::{Box, Vec};
+use core::iter::{IntoIterator, Iterator};
 use core::marker::PhantomData;
 use core::mem::{size_of, transmute};
 use core::ops::Index;
@@ -31,6 +32,16 @@ impl<T> GapArray<T> {
     pub unsafe fn set_layout(&mut self, size: usize, count: usize) {
         self.size = size;
         self.count = count;
+        if self.count > self.data.len() / self.size || self.size < size_of::<T>() {
+            panic!("invalid layout");
+        }
+    }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            array: self,
+            index: 0,
+        }
     }
 }
 
@@ -43,14 +54,35 @@ impl<T> Index<usize> for GapArray<T> {
             || start >= self.data.len()
             || start + size_of::<T>() > self.data.len()
         {
-            panic!("GapArray index out of bounds");
+            panic!("index out of bounds");
         }
         unsafe { transmute::<&u8, &T>(&self.data[index]) }
     }
 }
 
-/*
-impl <T> IntoIterator for GapArray<T> {
+impl<'a, T> IntoIterator for &'a GapArray<T> {
+    type Item = &'a T;
+    type IntoIter = Iter<'a, T>;
 
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
 }
-*/
+
+pub struct Iter<'a, T> {
+    array: &'a GapArray<T>,
+    index: usize,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.array.count {
+            return None;
+        }
+        let res = Some(&self.array[self.index]);
+        self.index += 1;
+        res
+    }
+}
